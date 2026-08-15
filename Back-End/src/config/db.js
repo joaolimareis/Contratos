f7a973a33c065e1656d7ea401d5254b6a829c938
joaolimareis <victorreislima186@gmail.com>
2026-08-15 19:58:59 -0300

    fix: adicionando dialectModule pg para o Vercel

--- Back-End/src/config/db.js
+++ Back-End/src/config/db.js
@@ -1,13 +1,15 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 import "dotenv/config";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    dialectModule: pg, // <-- Força o Sequelize a usar o pacote pg diretamente
     logging: false,
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false 
+            rejectUnauthorized: false
         }
     }
 });
