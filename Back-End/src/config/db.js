0971c25f6a1b39d29b61f4cfab5cf730533c96f3
joaolimareis <victorreislima186@gmail.com>
2026-08-15 20:31:50 -0300

    ix: corrigir configuração do Sequelize e entrypoint

--- Back-End/src/config/db.js
+++ Back-End/src/config/db.js
@@ -2,16 +2,17 @@ import { Sequelize } from "sequelize";
 import pg from "pg";
 import "dotenv/config";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    dialectModule: pg, // <-- Força o Sequelize a usar o pacote pg diretamente
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectModule: pg,
+        logging: false,
     }
-});
+);
 
 export default sequelize;
