3bf72f7a2056ddfb00440afacb4f9829dbd1fb9a
joaolimareis <victorreislima186@gmail.com>
2026-08-15 19:38:49 -0300

    deploy vercel

--- Back-End/src/config/db.js
+++ Back-End/src/config/db.js
@@ -1,18 +1,20 @@
 import { Sequelize } from "sequelize";
-
-
 import "dotenv/config";
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-
-    {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: console.log,
-    }
-);
-export default sequelize
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        logging: process.env.NODE_ENV !== "production",
+    })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: "postgres",
+            logging: process.env.NODE_ENV !== "production",
+        }
+    );
+export default sequelize;
