de2db5b87f8bec1be282ee3789a740b7f5a7d605
joaolimareis <victorreislima186@gmail.com>
2026-08-20 00:28:34 -0300

    banco nuvem NEON

--- Back-End/src/config/db.js
+++ Back-End/src/config/db.js
@@ -2,18 +2,30 @@ import { Sequelize } from "sequelize";
 import pg from "pg";
 import "dotenv/config";
 
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectModule: pg,
-    logging: console.log,
-  }
-);
+const sequelize =
+  process.env.DB_PROVIDER === "neon"
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        dialectModule: pg,
+        logging: console.log,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT,
+          dialect: "postgres",
+          dialectModule: pg,
+          logging: console.log,
+        }
+      );
 
 export default sequelize;
